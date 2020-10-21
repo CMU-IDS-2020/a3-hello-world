00ff88df8a61c389b78aa646961bd4a297721fd7
Xiaoyu <xsun256@wisc.edu>
2020-10-21 00:56:43 -0400

    switch role # 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,18 +9,6 @@ st.title("Happiness Data Analysis")
 def load_data(year, path="./data"):
     return pd.read_csv(os.path.join(path, str(year)) + "_map.csv")
 
-def show_correlation(features, df):
-    default_features = ['Rank', 'Score', 'GDP', 'Family', 'Health', 'Freedom', 'Generosity']
-    chart = alt.Chart(df).mark_point().encode(
-        x=alt.X(features[0], scale=alt.Scale(zero=True)),
-        y=alt.Y(features[1], scale=alt.Scale(zero=True)),
-        # show country name or region name and other attributes as tooltip
-        tooltip=['Country or Region'] + [f for f in default_sfeatures if f not in features]
-    ).properties(
-        width=700, height=400
-    ).interactive()
-    st.write(chart)
-
 features = st.multiselect(
     'Select features you are interested in', 
     ('Rank', 'Score', 'GDP', 'Family', 'Health', 'Freedom', 'Generosity'))
@@ -28,14 +16,30 @@ features = st.multiselect(
 if features:
     year = st.slider("Select Year", 2015, 2015, 2019, 1)
     df = load_data(year)
-    # st.write("Let's look at happeniss data in the Pandas Data Frame for year " + year + ".")
-    st.write(df)
-    # # TODO: show some basic information before going to attributes correlation?
-    # st.write("Interested in seeing attributes' correlations?")
-    # features = st.multiselect(
-    #     "Select two attributes to see correlation",
-    #     ('Rank', 'Score', 'GDP', 'Family', 'Health', 'Freedom', 'Generosity'))
-    # if (len(features) != 0) and (len(features) != 2):
-    #     st.write("Please select two attributes.")
-    # elif len(features) == 2:
-    #     show_correlation(features, df)
+    
+    brush = alt.selection_interval()  # selection of type "interval"
+    if st.checkbox("Show raw data"):
+        st.write(df)
+    
+
+    line_plot = alt.Chart(df).mark_line().encode(
+        x="Country or Region",
+        y="GDP"
+    ).properties(
+        width=700,
+        height = 100
+    ).interactive()
+    st.write(line_plot)
+
+    score_tick = alt.Chart(df).mark_tick().encode(
+        x='Score:Q',
+    ).encode(
+        color=alt.Color('Score:Q', scale=alt.Scale(scheme="oranges"))
+    ).properties(
+        width=700,
+        height = 100
+    ).add_selection(
+        brush
+    )
+    st.write(score_tick)
+
